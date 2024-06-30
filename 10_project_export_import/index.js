2b75ba5902280ad6206c960ee8e1b33fcd423a74
programmingElements <159023430+programmingElements@users.noreply.github.com>
2024-06-30 00:15:47 +0000

    export and import module

---
+++ 10_project_export_import/index.js
@@ -0,0 +1,15 @@
+const { add, sub, mult, name} = require('./calculate');
+
+console.log("Name : ", name);
+
+console.log("Addition: ");
+
+console.log(add(4, 6));
+
+console.log("Substraction: ")
+
+console.log(sub(6, 3));
+
+console.log("Multiplication: ")
+
+console.log(mult(3, 5));
